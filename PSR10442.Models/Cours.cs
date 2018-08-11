64ffdde7d23612aef6b522a3e7a7c855d89eeb0f
Bilal <elkhattouti.b@gmail.com>
2018-08-11 12:38:06 +0200

    Model 1

---
+++ PSR10442.Models/Cours.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PSR10442.Models
+{
+	[Table("Cours")]
+    public class Cours
+    {
+		[Key]
+		public int Id;
+
+		public string Nom;
+		public string Libele;
+
+    }
+}
